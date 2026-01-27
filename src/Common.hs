3869e7390c574c7b1ca53742cc34055d5159f148
Martin Sferco <martinsferco@hotmail.com>
2026-01-27 19:33:28 -0300

    adds common definitions

--- src/Common.hs
+++ src/Common.hs
@@ -9,3 +9,9 @@ type NodeIdent = String
 type Vars  = Set.Set VarIdent
 type Atoms = Set.Set AtomIdent
 type Nodes = Set.Set NodeIdent
+
+type Label = (NodeIdent, [AtomIdent])
+type Neighboors = [NodeIdent]
+type Transition = (NodeIdent, Bool, Neighboors)
+
+type Definition = (VarIdent, Type, Value)
