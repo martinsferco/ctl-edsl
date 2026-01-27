cfc356e396b367f950accf66197842719e38d337
Martin Sferco <martinsferco@hotmail.com>
2026-01-26 23:52:05 -0300

    implements the Sat solver

--- src/Sat.hs
+++ src/Sat.hs
@@ -1,66 +1,82 @@
 module Sat where
 
--- import AST
--- import qualified Data.Set as Set
+import AST
+import TSystem
+import Common 
 
--- -- Capaz lo hacemos monadico por el MEME
+import qualified Data.Set as Set
 
--- sat :: TSystem -> Formula -> Set.Set NodeIdent
--- sat = sat' . transform
---   where 
---     sat' _  F = Set.empty
---     sat' m (Atom a)             = undefined
---     sat' m (Not p)              = undefined
---     sat' m (BinaryOp And p q)   = undefined
---     sat' m (UQuantifier EC p)   = undefined
---     sat' m (UQuantifier AC p)   = undefined
---     sat' m (UQuantifier AR p)   = undefined
---     sat' m (BQuantifier EU p q) = undefined
---     sat' _ _  = error "It should be transformed already"
+
+sat :: TSystem -> Formula -> Nodes
+sat ts = (sat' ts) . transform
+  where 
+    sat' _  F                   = Set.empty
+    sat' _  T                   = getNodes ts
+    sat' m (Atom a)             = Set.filter (isLabel ts a) (getNodes ts)
+    sat' m (Not p)              = Set.difference (getNodes ts) (sat ts p)
+    sat' m (BinaryOp And p q)   = Set.intersection (sat ts p) (sat ts q)
+    sat' m (UQuantifier EC p)   = preExist ts (sat ts p)
+    sat' m (UQuantifier AC p)   = preForAll ts (sat ts p)
+    sat' m (UQuantifier AR p)   = inev ts (sat ts p)
+    sat' m (BQuantifier EU p q) = exUntil ts (sat ts p) (sat ts q)
+    sat' _ _                    = error "It should be transformed already"
 
 -- counterExample ::  TSystem -> Formula
 -- counterExample = undefined
 
 
--- transform :: Formula -> Formula
--- transform F = F
--- transform T = (Not F)
--- transform v@(Var _) = v -- Capaz buscar la variable????
--- transform a@(Atom _) = a
--- transform (BinaryOp And p q) = BinaryOp And (transform p) (transform q)
--- transform (BinaryOp Or p q) = let tp = Not (transform p)
---                                   tq = Not (transform q)
---                               in Not (BinaryOp And tp tq)
+transform :: Formula -> Formula
+transform F                      = F
+transform T                      = T
+transform a@(Atom _)             = a
+transform (BinaryOp And p q)     = BinaryOp And (transform p) (transform q)
+
+transform (BinaryOp Or p q)      = let tp = Not (transform p)
+                                       tq = Not (transform q)
+                                   in Not (BinaryOp And tp tq)
   
--- transform (BinaryOp Implies p q) = let tp = transform p
---                                        tq = Not (transform q)
---                                    in Not (BinaryOp And tp tq)
+transform (BinaryOp Implies p q) = let tp = transform p
+                                       tq = Not (transform q)
+                                   in Not (BinaryOp And tp tq)
   
--- transform (UQuantifier AC p) = undefined
--- transform (UQuantifier AC p) = undefined
--- transform (UQuantifier AC p) = undefined
--- transform (UQuantifier AC p) = undefined
--- transform (UQuantifier AC p) = undefined
--- transform (UQuantifier AC p) = undefined
--- transform (BQuantifier AU p q) = undefined
--- transform (BQuantifier EU p q) = undefined
+transform (UQuantifier AC p)     = UQuantifier AC $ transform p
+transform (UQuantifier EC p)     = UQuantifier EC $ transform p
+transform (UQuantifier AR p)     = UQuantifier AR $ transform p
+transform (UQuantifier ER p)     = BQuantifier EU T $ transform p
+transform (UQuantifier AS p)     = Not (BQuantifier EU T (Not $ transform p))
+transform (UQuantifier ES p)     = Not (UQuantifier AR (Not $ transform p))
+
+transform (BQuantifier AU p q)   = let tp = transform p
+                                       tq = transform q
+                                       neitherPQ = BinaryOp And (Not tp) (Not tq)
+                                       untilPath = Not (BQuantifier EU (Not tq) neitherPQ) 
+                                       inevitableQ = UQuantifier AR $ transform q
+                                   in BinaryOp And untilPath inevitableQ
+
+transform (BQuantifier EU p q)   = BQuantifier EU (transform p) (transform q)
 
 
--- inev :: TSystem -> Set.Set NodeIdent -> Set.Set NodeIdent
--- inev nodes = undefined
+inev :: TSystem -> Nodes -> Nodes
+inev ts nodes = let nodes' = Set.union nodes (preForAll ts nodes)
+                in if nodes' == nodes then nodes
+                                      else inev ts nodes'
 
--- exUntil :: TSystem -> Set.Set NodeIdent -> Set.Set NodeIdent
--- exUntil m  = undefined
+exUntil :: TSystem -> Nodes -> Nodes -> Nodes
+exUntil ts x y  = let y' = Set.union y (Set.intersection x (preExist ts y))
+                  in if y' == y then y else exUntil ts x y'
 
--- forAllUntil :: TSystem -> Set.Set NodeIdent -> Set.Set NodeIdent
--- forAllUntil = undefined
 
--- -- filtro todos los nodos del sistema, para los cuales existe 
--- -- una transicion dentro de NODES
--- preExist :: TSystem -> Set.Set NodeIdent -> Set.Set NodeIdent
--- preExist ts nodes = undefined
+forAllUntil :: TSystem -> Nodes -> Nodes -> Nodes
+forAllUntil = undefined
 
--- -- filtro todos los nodos del sistema, para los cuales todas 
--- -- sus transiciones dentro de NODES
--- preForAll :: TSystem -> Set.Set NodeIdent -> Set.Set NodeIdent
--- preForAll ts nodes = undefined
+preExist :: TSystem -> Nodes -> Nodes
+preExist ts nodes = Set.filter existS (getNodes ts)
+  where
+    existS :: NodeIdent -> Bool
+    existS node = not $ Set.disjoint (getNeighboors ts node) nodes
+
+preForAll :: TSystem -> Nodes -> Nodes
+preForAll ts nodes = Set.filter forAllS $ (getNodes ts)
+  where 
+    forAllS :: NodeIdent -> Bool
+    forAllS node = Set.isSubsetOf (getNeighboors ts node) nodes
