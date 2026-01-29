91bc472749f74f108fbbc753c9c8aafe80717ddd
Martin Sferco <martinsferco@hotmail.com>
2026-01-28 21:24:05 -0300

    modifies errors

--- src/Error.hs
+++ src/Error.hs
@@ -4,6 +4,14 @@ import Text.Parsec.Error ( ParseError )
 
 data Error 
   = ParseErr ParseError
-  | TypeErr 
-  | EvalErr
-  deriving (Show, Eq)
+  | TypecheckError String
+  | RuntimeError String 
+  | StateError String
+  | ModelError String
+
+instance Show Error where
+  show (ParseErr e)       = show e
+  show (TypecheckError s) = "type check error: " ++ s
+  show (RuntimeError s)   = "runtime  error: " ++ s
+  show (StateError s)     = "state error: " ++ s
+  show (ModelError s)     = "model error: " ++ s
