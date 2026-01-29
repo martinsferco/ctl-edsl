589ee85d2ebd1da029fd560e6f8645190185e599
Martin Sferco <martinsferco@hotmail.com>
2026-01-29 18:10:01 -0300

    Sat and some methods of TSystem now are monadic

--- src/Model/TSystemMethods.hs
+++ src/Model/TSystemMethods.hs
@@ -6,18 +6,26 @@ import Common
 
 import qualified Data.Map as Map
 import qualified Data.Set as Set
-  
+
+import Control.Monad (unless)
 
 buildTSystem :: MonadCTL m => InfoNodes -> LabelingFunction -> m TSystem
 buildTSystem infoNodes labeling = 
   do g <- buildGraph infoNodes
      nonBlockingGraph g
-     nodeCoherence g labeling
-     return $ TSystem g labeling
+     labelCoherence g labeling
+     return $ TSystem g (completeLabeling g labeling)
 
-nodeCoherence :: MonadCTL m => Graph -> LabelingFunction -> m ()
-nodeCoherence g l = if nodes g == Map.keysSet l then return ()
-                                                else failCTL "incoherence"
+labelCoherence :: MonadCTL m => Graph -> LabelingFunction -> m ()
+labelCoherence graph labels = unless (Map.keysSet labels `Set.isSubsetOf` nodes graph)
+                                      (modelError "a labeling function is refering to a node not defined in the graph")
+
+completeLabeling :: Graph -> LabelingFunction -> LabelingFunction
+completeLabeling graph labels = 
+  let allNodes = nodes graph
+      labeledNodes = Map.keysSet labels
+      notLabeledNodes = Set.difference allNodes labeledNodes
+  in Map.union (Map.fromSet (const Set.empty) notLabeledNodes) labels
 
 getNodes :: TSystem -> Nodes
 getNodes = nodes . graph
@@ -25,19 +33,21 @@ getNodes = nodes . graph
 getInitialNodes :: TSystem -> Nodes
 getInitialNodes = inits . graph
 
--- puede fallar si no exite el nodo
-getNeighboors :: TSystem -> NodeIdent -> Nodes
-getNeighboors ts node = ((.) trans graph ts) Map.! node 
+getNeighboors :: MonadCTL m => TSystem -> NodeIdent -> m Nodes
+getNeighboors ts node = node `isNodeOf` ts >>
+                        (return $ ((.) trans graph ts) Map.! node)
+                       
+getLabels :: MonadCTL m => TSystem -> NodeIdent -> m Atoms
+getLabels ts node = node `isNodeOf` ts >>
+                    (return $ (labeling ts) Map.! node)
 
--- puede fallar si no exite el nodo
-getLabels :: TSystem -> NodeIdent -> Atoms
-getLabels ts node = (labeling ts) Map.! node 
+isLabel :: MonadCTL m => TSystem -> AtomIdent -> NodeIdent -> m Bool
+isLabel ts atom node =  node `isNodeOf` ts >>
+                        (Set.member atom <$> getLabels ts node)
 
--- puede fallar si no existe el nodo
-isLabel :: TSystem -> AtomIdent -> NodeIdent -> Bool
-isLabel ts atom node = Set.member atom (getLabels ts node)
-
-
+isNodeOf :: MonadCTL m => NodeIdent -> TSystem -> m ()
+isNodeOf node ts =  unless (node `Set.member` (getNodes ts))
+                           (modelError $ (show node) ++ " is not a node in the transition system")
 
 buildGraph :: MonadCTL m => InfoNodes -> m Graph
 buildGraph (InfoNodes init trans) = return $ Graph allNodes init trans
@@ -50,3 +60,7 @@ nonBlockingGraph graph = if nonBlocking then return ()
   where 
     nonBlocking = nodes graph == Map.keysSet (trans graph)
 
+
+
+exportTSystem :: MonadCTL m => TSystem -> m ()
+exportTSystem ts = return ()
--- src/Sat.hs
+++ src/Sat.hs
@@ -4,42 +4,47 @@ import AST
 import Model.TSystem
 import Model.TSystemMethods
 import Common 
-
 import MonadCTL
 
+import Control.Monad (filterM, join)
 import qualified Data.Set as Set
 
 
 models :: MonadCTL m => TSystem -> Formula -> m ()
-models ts form = let satNodes = sat ts form
-                 in if getInitialNodes ts `Set.isSubsetOf` satNodes
+models ts form = do satNodes <- sat ts form
+                    if getInitialNodes ts `Set.isSubsetOf` satNodes
                     then printCTL "the model models the formula"
-                    else printCTL "the model does not models the formula"
+                    else printCTL "the model does not models the formula∀"
 
 isSatis :: MonadCTL m => Formula -> m ()
 isSatis form = failCTL "isSatis is not implemented"
 
 isValid :: MonadCTL m => TSystem -> NodeIdent -> Formula -> m ()
-isValid ts node form = let satNodes = sat ts form
-                       in if node `Set.member` satNodes
+isValid ts node form = do satNodes <- sat ts form
+                          if node `Set.member` satNodes
                           then printCTL "is valid"
                           else printCTL "is not valid" 
 
+counterExample :: t
+counterExample = undefined
 
+witness :: t 
+witness = undefined
 
-sat :: TSystem -> Formula -> Nodes
+sat :: MonadCTL m => TSystem -> Formula -> m Nodes
 sat ts = (sat' ts) . transform
   where 
-    sat' _  F                   = Set.empty
-    sat' _  T                   = getNodes ts
-    sat' m (Atom a)             = Set.filter (isLabel ts a) (getNodes ts)
-    sat' m (Not p)              = Set.difference (getNodes ts) (sat ts p)
-    sat' m (BinaryOp And p q)   = Set.intersection (sat ts p) (sat ts q)
-    sat' m (UQuantifier EC p)   = preExist ts (sat ts p)
-    sat' m (UQuantifier AC p)   = preForAll ts (sat ts p)
-    sat' m (UQuantifier AR p)   = inev ts (sat ts p)
-    sat' m (BQuantifier EU p q) = exUntil ts (sat ts p) (sat ts q)
-    sat' _ _                    = error "It should be transformed already"
+    sat' :: MonadCTL m => TSystem -> Formula -> m Nodes
+    sat' _  F                   = return Set.empty
+    sat' m  T                   = return $ getNodes m
+    sat' m (Atom a)             = setFilterM (isLabel m a) (getNodes m)
+    sat' m (Not p)              = Set.difference (getNodes m) <$> (sat' m p)
+    sat' m (BinaryOp And p q)   = Set.intersection <$> (sat' m p) <*> (sat' m q)
+    sat' m (UQuantifier EC p)   = sat' m p >>= preExist m
+    sat' m (UQuantifier AC p)   = sat' m p >>= preForAll m
+    sat' m (UQuantifier AR p)   = sat' m p >>= inev m
+    sat' m (BQuantifier EU p q) = join (exUntil m <$> sat' m p <*> sat' m q)
+    sat' _ _                    = satError "the formula should be already trasnformed"
 
 
 transform :: Formula -> Formula
@@ -49,13 +54,13 @@ transform a@(Atom _)             = a
 transform (Not p)                = Not (transform p)
 transform (BinaryOp And p q)     = BinaryOp And (transform p) (transform q)
 
-transform (BinaryOp Or p q)      = let tp = Not (transform p)
-                                       tq = Not (transform q)
-                                   in Not (BinaryOp And tp tq)
+transform (BinaryOp Or p q)      =  let tp = Not (transform p)
+                                        tq = Not (transform q)
+                                    in Not (BinaryOp And tp tq)
   
-transform (BinaryOp Implies p q) = let tp = transform p
-                                       tq = Not (transform q)
-                                   in Not (BinaryOp And tp tq)
+transform (BinaryOp Implies p q) =  let tp = transform p
+                                        tq = Not (transform q)
+                                    in Not (BinaryOp And tp tq)
   
 transform (UQuantifier AC p)     = UQuantifier AC $ transform p
 transform (UQuantifier EC p)     = UQuantifier EC $ transform p
@@ -64,37 +69,72 @@ transform (UQuantifier ER p)     = BQuantifier EU T $ transform p
 transform (UQuantifier AS p)     = Not (BQuantifier EU T (Not $ transform p))
 transform (UQuantifier ES p)     = Not (UQuantifier AR (Not $ transform p))
 
-transform (BQuantifier AU p q)   = let tp = transform p
-                                       tq = transform q
-                                       neitherPQ = BinaryOp And (Not tp) (Not tq)
-                                       untilPath = Not (BQuantifier EU (Not tq) neitherPQ) 
-                                       inevitableQ = UQuantifier AR $ transform q
-                                   in BinaryOp And untilPath inevitableQ
+transform (BQuantifier AU p q)   =  let tp = transform p
+                                        tq = transform q
+                                        neitherPQ = BinaryOp And (Not tp) (Not tq)
+                                        untilPath = Not (BQuantifier EU (Not tq) neitherPQ) 
+                                        inevitableQ = UQuantifier AR $ transform q
+                                    in BinaryOp And untilPath inevitableQ
 
 transform (BQuantifier EU p q)   = BQuantifier EU (transform p) (transform q)
 
 
-inev :: TSystem -> Nodes -> Nodes
-inev ts nodes = let nodes' = Set.union nodes (preForAll ts nodes)
-                in if nodes' == nodes then nodes
+inev :: MonadCTL m => TSystem -> Nodes -> m Nodes
+inev ts nodes = do nodes' <- (Set.union nodes <$> (preForAll ts nodes))
+                   if nodes' == nodes then return nodes
                                       else inev ts nodes'
 
-exUntil :: TSystem -> Nodes -> Nodes -> Nodes
-exUntil ts x y  = let y' = Set.union y (Set.intersection x (preExist ts y))
-                  in if y' == y then y else exUntil ts x y'
+exUntil :: MonadCTL m => TSystem -> Nodes -> Nodes -> m Nodes
+exUntil ts x y  = do y' <- Set.union y <$> (Set.intersection x <$> (preExist ts y))
+                     if y' == y then return y  
+                                else exUntil ts x y'
 
-
-forAllUntil :: TSystem -> Nodes -> Nodes -> Nodes
-forAllUntil = undefined
-
-preExist :: TSystem -> Nodes -> Nodes
-preExist ts nodes = Set.filter existS (getNodes ts)
+preExist :: MonadCTL m => TSystem -> Nodes -> m Nodes
+preExist ts nodes = setFilterM existS (getNodes ts)
   where
-    existS :: NodeIdent -> Bool
-    existS node = not $ Set.disjoint (getNeighboors ts node) nodes
+    existS :: MonadCTL m => NodeIdent -> m Bool
+    existS node = not <$> (Set.disjoint <$> (getNeighboors ts node) <*> pure nodes)
 
-preForAll :: TSystem -> Nodes -> Nodes
-preForAll ts nodes = Set.filter forAllS $ (getNodes ts)
+preForAll :: MonadCTL m => TSystem -> Nodes -> m Nodes
+preForAll ts nodes = setFilterM forAllS (getNodes ts)
   where 
-    forAllS :: NodeIdent -> Bool
-    forAllS node = Set.isSubsetOf (getNeighboors ts node) nodes
+    forAllS :: MonadCTL m => NodeIdent -> m Bool
+    forAllS node = Set.isSubsetOf <$> (getNeighboors ts node) <*> pure nodes
+
+
+getAtoms :: Formula -> Atoms
+getAtoms F                   = Set.empty 
+getAtoms T                   = Set.empty
+getAtoms (Atom atom)         = Set.singleton atom
+getAtoms (Not p)             = getAtoms p
+getAtoms (BinaryOp _ p q)    = Set.union (getAtoms p) (getAtoms q)
+getAtoms (UQuantifier _ p)   = getAtoms p
+getAtoms (BQuantifier _ p q) = Set.union (getAtoms p) (getAtoms q)
+
+-- allTSystems :: Atoms -> [TSystem]
+-- allTSystems atoms = tSystems atoms 1
+
+-- tSystems :: Atoms -> Int -> [TSystem]
+-- tSystems atoms n = tSystemGenerator atoms n ++ tSystems atoms (n + 1)
+
+-- generates all the TSystems of size N with labels in Atoms
+-- tSystemGenerator :: Atoms -> Int -> [TSystem]
+-- tSystemGenerator atoms size = 
+--   let nodes     = map (\n -> "_n" ++ show n) [1..size]
+--       allLabels = map () nodes 
+ -- para cada nodo construyo lo siguiente
+ --  - un listado o conjunto de todas las posibles formas de etiquetarlo
+ --  - un listado o conjunto de todas las transiciones posibles
+ --
+ -- cuando tengo eso de cada nodo, basicamente voy tomando uno de cada, y voy formando TSystem nuevos
+
+--  allPossibleLabels :: Atoms -> [Atoms]
+--  allPossibleLabels atoms = subsequences atoms
+
+--  allPossibleTransitions :: Nodes -> [Nodes]
+--  allPossibleTransitions nodes = subsequences nodes
+ 
+
+setFilterM :: (Ord a, MonadCTL m) => (a -> m Bool) -> Set.Set a -> m (Set.Set a)
+setFilterM p = fmap Set.fromList . filterM p . Set.toList
+  
