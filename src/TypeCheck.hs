2d4bcb4a11fef2104f34ae6322a396e5d6b705f6
Martin Sferco <martinsferco@hotmail.com>
2026-01-28 22:41:39 -0300

    improves the tc

--- src/TypeCheck.hs
+++ src/TypeCheck.hs
@@ -7,30 +7,41 @@ import MonadCTL
 
 import qualified Data.Set as Set
 
-ofType :: MonadCTL m => Expr -> Type -> m()
-ofType expr ty = do exprTy <- findType expr
-                    expectType exprTy ty
+exprOfType :: MonadCTL m => Expr -> Type -> m()
+exprOfType expr ty = do exprTy <- findTypeExpr expr
+                        expectType exprTy ty
+
+valueOfType :: MonadCTL m => Value -> Type -> m()
+valueOfType value ty = do valueTy <- findTypeValue value
+                          expectType valueTy ty
 
 expectType :: MonadCTL m => Type -> Type -> m()      
-expectType ty1 ty2 = if ty1 == ty2 then return ()
+expectType ty1 ty2 = if ty1 == ty2 then failCTL "jeje"
                                    else failCTL "no matchean los tipos"
 
-findType :: MonadCTL m => Expr -> m Type
-findType (ModelExpr t l)   = do ofType t NodesTy
-                                ofType l LabelsTy
-                                return ModelTy
-findType (LabelsExpr _)      = return LabelsTy
-findType (NodesExpr _) = return NodesTy
+findTypeExpr :: MonadCTL m => Expr -> m Type
+findTypeExpr (ModelExpr t l)   = do exprOfType t NodesTy
+                                    exprOfType l LabelsTy
+                                    return ModelTy
+findTypeExpr (LabelsExpr _)      = return LabelsTy
+findTypeExpr (NodesExpr _) = return NodesTy
 
-findType (VarExpr var)      = getTy var
+findTypeExpr (VarExpr var)      = getTy var
 
-findType (FormulaExpr f)   = do let fv = Set.toList $ freeVariables f
-                                mapM_ checkFormulaVarTy fv
-                                return FormulaTy
+findTypeExpr (FormulaExpr f)   = do let fv = Set.toList $ freeVariables f
+                                    mapM_ checkFormulaVarTy fv
+                                    return FormulaTy
 
     where checkFormulaVarTy :: MonadCTL m => VarIdent -> m ()
           checkFormulaVarTy var = do varTy <- getTy var
                                      expectType varTy FormulaTy
+
+
+findTypeValue :: MonadCTL m => Value -> m Type
+findTypeValue (Model _)   = return ModelTy
+findTypeValue (Labels _)  = return LabelsTy
+findTypeValue (Formula _) = return FormulaTy
+findTypeValue (Nodes _)   = return NodesTy
 
 freeVariables :: SFormula -> Set.Set VarIdent
 freeVariables SF                   = Set.empty
@@ -51,8 +62,8 @@ freeVariables (SBQuantifier _ p q) = let varP = freeVariables p
 freeVariables (SVar var)           = Set.singleton var
 
 typeCheckSentence :: MonadCTL m => Sentence -> m() 
-typeCheckSentence (Def _ ty expr)        = expr `ofType` ty 
-typeCheckSentence (Export model)         = model `ofType` ModelTy
-typeCheckSentence (IsValid formula)      = formula `ofType` FormulaTy
-typeCheckSentence (Models model formula) = do model `ofType` ModelTy
-                                              formula `ofType` FormulaTy
+typeCheckSentence (Def _ ty expr)        = expr `exprOfType` ty 
+typeCheckSentence (Export model)         = model `exprOfType` ModelTy
+typeCheckSentence (IsSatis formula)      = formula `exprOfType` FormulaTy
+typeCheckSentence (Models model formula) = do model `exprOfType` ModelTy
+                                              formula `exprOfType` FormulaTy
